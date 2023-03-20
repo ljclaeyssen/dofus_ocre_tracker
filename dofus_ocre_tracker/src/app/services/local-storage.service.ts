61745305229ad6dc814e7c7502dbdce18149b274
ljcla <ljclaeyssen@gmail.com>
2023-03-20 21:09:55 +0100

    set up

---
+++ dofus_ocre_tracker/src/app/services/local-storage.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LocalStorageService {
+
+  constructor() {  }
+
+  public read<T>(key: string): T | null {
+    const storedItem = localStorage.getItem(key);
+    if (storedItem) {
+      return JSON.parse(storedItem) as T;
+    }
+    return null;
+  }
+
+  public write(key: string, item: any): void {
+    localStorage.setItem(key, JSON.stringify(item));
+  }
+}
